24b6458eee88b37e8c55119a0510aaea5a4839dc
Adrian Kuta <redione193@gmail.com>
2015-07-05 14:39:03 +0200

    Left drawer

--- app/src/main/java/com/myapps/myrecipes/MainActivity.java
+++ app/src/main/java/com/myapps/myrecipes/MainActivity.java
@@ -1,17 +1,25 @@
 package com.myapps.myrecipes;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		findViewById(R.id.start_button).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				startActivity(new Intent(MainActivity.this, NaviagtionDrawerActivity.class));
+			}
+		});
 	}
 
 	@Override
---
+++ app/src/main/java/com/myapps/myrecipes/NaviagtionDrawerActivity.java
@@ -0,0 +1,80 @@
+package com.myapps.myrecipes;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class NaviagtionDrawerActivity extends AppCompatActivity
+		implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+
+	/**
+	 * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
+	 */
+	private NavigationDrawerFragment mNavigationDrawerFragment;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_naviagtion_drawer);
+		setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+		mNavigationDrawerFragment = (NavigationDrawerFragment)
+				getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
+
+		// Set up the drawer.
+		mNavigationDrawerFragment.setUp(
+				R.id.navigation_drawer,
+				(DrawerLayout) findViewById(R.id.drawer_layout));
+	}
+
+	@Override
+	public void onNavigationDrawerItemSelected(int position) {
+		// update the main content by replacing fragments
+		Fragment fragment = getFragment(position);
+		FragmentManager fragmentManager = getSupportFragmentManager();
+		fragmentManager.beginTransaction()
+				.replace(R.id.container, fragment)
+				.commit();
+	}
+
+	private Fragment getFragment(int position) {
+		switch (position) {
+			case 0:
+				return TopRatedRecipesFragment.newInstance();
+			default:
+				return TopRatedRecipesFragment.newInstance();
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		if (!mNavigationDrawerFragment.isDrawerOpen()) {
+			// Only show items in the action bar relevant to this screen
+			// if the drawer is not showing. Otherwise, let the drawer
+			// decide what to show in the action bar.
+			getMenuInflater().inflate(R.menu.naviagtion_drawer, menu);
+			return true;
+		}
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// Handle action bar item clicks here. The action bar will
+		// automatically handle clicks on the Home/Up button, so long
+		// as you specify a parent activity in AndroidManifest.xml.
+		int id = item.getItemId();
+
+		//noinspection SimplifiableIfStatement
+		if (id == R.id.action_settings) {
+			return true;
+		}
+
+		return super.onOptionsItemSelected(item);
+	}
+}
