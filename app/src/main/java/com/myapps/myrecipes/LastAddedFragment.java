12c15a3278f910f8d25838c649da081af4ba2f5c
Adrian Kuta <redione193@gmail.com>
2015-10-26 16:34:02 +0100

    Same parent class and layout for fragments which displaying list of recipes

--- app/src/main/java/com/myapps/myrecipes/LastAddedFragment.java
+++ app/src/main/java/com/myapps/myrecipes/LastAddedFragment.java
@@ -1,35 +1,18 @@
 package com.myapps.myrecipes;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.GridView;
-import android.widget.Toast;
-
-import com.myapps.myrecipes.parseobjects.Recipe;
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseQuery;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class LastAddedFragment extends Fragment {
+public class LastAddedFragment extends MyAllRecipesFragment {
 
 
-	private GridAdapter gridAdapter;
-	private List<Recipe> recipeList;
-	private ParseQuery<Recipe> query;
 
 	@NonNull
 	public static LastAddedFragment newInstance() {
@@ -37,60 +20,9 @@ public class LastAddedFragment extends Fragment {
 	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-	                         Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_last_added, container, false);
-	}
-
-	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 
-		GridView observableGridView = (GridView) view.findViewById(R.id.last_added_gridView);
-		observableGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				Recipe recipe = recipeList.get(position);
-				Intent intent = new Intent(getActivity(), RecipeActivity.class);
-				intent.putExtra(RecipeActivity.RECIPE_ID, recipe.getObjectId());
-				intent.putExtra(RecipeActivity.RECIPE_NAME, recipe.getName());
-				intent.putExtra(RecipeActivity.CATEGORY, recipe.getCategory());
-				intent.putExtra(RecipeActivity.DIFFICULTY, recipe.getDifficulty());
-				intent.putExtra(RecipeActivity.INGREDIENTS, recipe.getIngredientJSON());
-				intent.putExtra(RecipeActivity.PHOTO_URL, recipe.getPhotoUrl());
-				startActivity(intent);
-			}
-		});
-		recipeList = new ArrayList<>();
-		gridAdapter = new GridAdapter(getActivity(), recipeList);
-		observableGridView.setAdapter(gridAdapter);
-		query = Recipe.getQuery();
 		query.orderByDescending("createdAt");
-		query.findInBackground(new FindCallback<Recipe>() {
-			@Override
-			public void done(List<Recipe> list, ParseException e) {
-				if (e != null) {
-					Toast.makeText(getActivity(),
-							"Error saving: " + e.getMessage(),
-							Toast.LENGTH_LONG).show();
-					query.fromLocalDatastore();
-					query.findInBackground(new FindCallback<Recipe>() {
-						@Override
-						public void done(List<Recipe> list, ParseException e) {
-							if (e == null) {
-								recipeList.clear();
-								recipeList.addAll(list);
-								gridAdapter.notifyDataSetChanged();
-							}
-						}
-					});
-				} else {
-					recipeList.clear();
-					recipeList.addAll(list);
-					gridAdapter.notifyDataSetChanged();
-				}
-			}
-		});
 	}
 }
--- app/src/main/java/com/myapps/myrecipes/MyRecipesFragment.java
+++ app/src/main/java/com/myapps/myrecipes/MyRecipesFragment.java
@@ -1,36 +1,18 @@
 package com.myapps.myrecipes;
 
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.GridView;
-import android.widget.Toast;
 
-import com.myapps.myrecipes.parseobjects.Recipe;
-import com.parse.FindCallback;
-import com.parse.ParseException;
-import com.parse.ParseQuery;
 import com.parse.ParseUser;
-
-import java.util.ArrayList;
-import java.util.List;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class MyRecipesFragment extends Fragment {
-
-
-	private List<Recipe> recipeList;
-	private GridAdapter gridAdapter;
-	private ParseQuery<Recipe> query;
+public class MyRecipesFragment extends MyAllRecipesFragment {
 
 	public MyRecipesFragment() {
 		// Required empty public constructor
@@ -42,62 +24,11 @@ public class MyRecipesFragment extends Fragment {
 	}
 
 	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container,
-	                         Bundle savedInstanceState) {
-		// Inflate the layout for this fragment
-		return inflater.inflate(R.layout.fragment_my_recipes, container, false);
-	}
-
-	@Override
 	public void onViewCreated(View view, Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 		ParseUser parseUser = ParseUser.getCurrentUser();
 		if (parseUser == null)
 			return;
-		GridView observableGridView = (GridView) view.findViewById(R.id.my_recipes_gridView);
-		observableGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-			@Override
-			public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-				Recipe recipe = recipeList.get(position);
-				Intent intent = new Intent(getActivity(), RecipeActivity.class);
-				intent.putExtra(RecipeActivity.RECIPE_ID, recipe.getObjectId());
-				intent.putExtra(RecipeActivity.RECIPE_NAME, recipe.getName());
-				intent.putExtra(RecipeActivity.CATEGORY, recipe.getCategory());
-				intent.putExtra(RecipeActivity.DIFFICULTY, recipe.getDifficulty());
-				intent.putExtra(RecipeActivity.INGREDIENTS, recipe.getIngredientJSON());
-				intent.putExtra(RecipeActivity.PHOTO_URL, recipe.getPhotoUrl());
-				startActivity(intent);
-			}
-		});
-		recipeList = new ArrayList<>();
-		gridAdapter = new GridAdapter(getActivity(), recipeList);
-		observableGridView.setAdapter(gridAdapter);
-		query = Recipe.getQuery();
 		query.whereEqualTo("author", parseUser);
-		query.findInBackground(new FindCallback<Recipe>() {
-			@Override
-			public void done(List<Recipe> list, ParseException e) {
-				if (e != null) {
-					Toast.makeText(getActivity(),
-							"Error saving: " + e.getMessage(),
-							Toast.LENGTH_LONG).show();
-					query.fromLocalDatastore();
-					query.findInBackground(new FindCallback<Recipe>() {
-						@Override
-						public void done(List<Recipe> list, ParseException e) {
-							if (e == null) {
-								recipeList.clear();
-								recipeList.addAll(list);
-								gridAdapter.notifyDataSetChanged();
-							}
-						}
-					});
-				} else {
-					recipeList.clear();
-					recipeList.addAll(list);
-					gridAdapter.notifyDataSetChanged();
-				}
-			}
-		});
 	}
 }
