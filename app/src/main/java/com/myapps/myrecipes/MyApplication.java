e02f7a83be7a377683e9ec6cea1412bc08a8839e
Adrian Kuta <Adrian Kuta>
2016-01-13 14:45:24 +0100

    .

--- app/src/main/java/com/myapps/myrecipes/MyApplication.java
+++ app/src/main/java/com/myapps/myrecipes/MyApplication.java
@@ -12,6 +12,7 @@ import com.myapps.myrecipes.parseobjects.Rating;
 import com.myapps.myrecipes.parseobjects.Recipe;
 import com.parse.Parse;
 import com.parse.ParseFacebookUtils;
+import com.parse.ParseInstallation;
 import com.parse.ParseObject;
 import com.parse.ParseUser;
 
@@ -34,6 +35,7 @@ public class MyApplication extends Application {
 		ParseUser.registerSubclass(MyParseUser.class);
 		Parse.enableLocalDatastore(this);
 		Parse.initialize(this, "sOtPvBFE5R8JWOcfm2gPFAUlNQLjFgadZ9KiQJMj", "jI625qcX1LCCoKaKBSMJa9dNWGzfgFhZBu3Zw5p3");
+		ParseInstallation.getCurrentInstallation().saveInBackground();
 		ParseFacebookUtils.initialize(this);
 		PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
 		FacebookSdk.sdkInitialize(this);
